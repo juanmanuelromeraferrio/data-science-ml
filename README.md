183ea66f85a0c4fef3bc5a915ddd5e8403bbbaf9
Juan M. Romera <juanmanuel.romeraferrio@gmail.com>
2017-07-13 20:12:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Data Science ML
+
+En este repositorio voy guardando ejercicios y aprendizaje del libro Data Science from Scratch de Joel Grus y del Curso de Machine Learning de EDX
---
+++ src/exercises/probability.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 11 21:44:50 2017
+
+@author: Juan Manuel Romera, based on Joel Grus code from data-science-from-scratch 
+"""
+
+import math
+
+def normal_approximation_to_binomial(n, p):
+    """finds mu and sigma corresponding to a Binomial(n, p)"""
+    mu = p * n
+    sigma = math.sqrt(p * (1 - p) * n)
+    return mu, sigma
+
+
+def normal_pdf(x, mu=0, sigma=1):
+    sqrt_two_pi = math.sqrt(2 * math.pi)
+    return (math.exp(-(x-mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
+
+def normal_cdf(x, mu=0,sigma=1):
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2  
+
+normal_probability_below = normal_cdf
+
+# it's between if it's less than hi, but not less than lo
+def normal_probability_between(lo, hi, mu=0, sigma=1):
+    return normal_cdf(hi, mu, sigma) - normal_cdf(lo, mu, sigma)
+
+# it's above the threshold if it's not below the threshold
+def normal_probability_above(lo, mu=0, sigma=1):
+    return 1 - normal_cdf(lo, mu, sigma)
